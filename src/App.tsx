9b1859a070ace549dc00568cf448514fac65058c
taichi.murai <taichimurai0514@gmail.com>
2019-10-27 21:46:25 +0900

    firebase 読み込み

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, FC } from 'react';
 import './App.css';
+import firebase from './plugins/firebase';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          deploy
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Hello: FC = () => <div>Hello!</div>;
+const logProps = (WrappedComponent: FC) => {
+  return class extends Component {
+    componentDidMount() {
+      firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+          console.log("ログイン中")
+        } else {
+          console.log("ログアウト中")
+        }
+      });
+    }
+    render() {
+      return <WrappedComponent />;
+    }
+  };
+};
 
-export default App;
+export default logProps(Hello);
---
+++ src/plugins/firebase.ts
@@ -0,0 +1,17 @@
+import * as firebase from 'firebase/app';
+import 'firebase/auth'
+
+export const config = {
+    apiKey: "AIzaSyAgyqSLvhYC_O3hb6kWlpFpVf-fziH_vUA",
+    authDomain: "reacttstodo.firebaseapp.com",
+    databaseURL: "https://reacttstodo.firebaseio.com",
+    projectId: "reacttstodo",
+    storageBucket: "reacttstodo.appspot.com",
+    messagingSenderId: "852105001616",
+    appId: "1:852105001616:web:76512886e32687f3923615",
+    measurementId: "G-NZXWR92ERD"
+};
+
+firebase.initializeApp(config)
+
+export default firebase
