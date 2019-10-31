e2d0da2be87d42189b980b11277a0ef30a9eb710
Taichi Murai <taichimurai0514@gmail.com>
2019-10-31 10:13:53 +0900

    [WIP] add login

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,44 @@
-import React, { Component, FC } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import firebase from './plugins/firebase';
-
-const Hello: FC = () => <div>Hello!</div>;
-const logProps = (WrappedComponent: FC) => {
-  return class extends Component {
-    componentDidMount() {
-      firebase.auth().onAuthStateChanged(user => {
-        if (user) {
-          console.log("ログイン中")
-        } else {
-          console.log("ログアウト中")
-        }
-      });
-    }
-    render() {
-      return <WrappedComponent />;
-    }
-  };
+interface Outh {
+  email: string; password: string
+}
+class hoge extends Component<{}, Outh> {
+  constructor(props: {}) {
+    super(props);
+    this.state = { email: "", password: "" }
+  }
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        console.log("ログイン中")
+      } else {
+        console.log("ログアウト中")
+      }
+    });
+  }
+  Login = () => {
+    console.log("login")
+    const { email, password } = this.state;
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ...
+    });
+  }
+  render() {
+    const { email, password } = this.state;
+    return (
+      <>
+        <input id="email" type="email" placeholder="メールアドレスを入力" value={email} />
+        <input id="password" type="password" placeholder="パスワードを入力" value={password} />
+        <button type="button" onClick={this.Login}>Login</button><button type="button">Logout</button>
+        {email}
+      </>
+    );
+  }
 };
 
-export default logProps(Hello);
+export default hoge;
