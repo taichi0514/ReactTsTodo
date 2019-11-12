255a88556dcb8e81529a06b46ba2140733b27382
Taichi Murai <murai@tam-bourine.co.jp>
2019-11-12 18:41:28 +0900

    add emailSet,passwordSet

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -23,17 +23,26 @@ class hoge extends Component<{}, Outh> {
     const { email, password } = this.state;
     firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
       // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
+      // const errorCode = error.code;
+      // const errorMessage = error.message;
       // ...
     });
   }
+
+  emailSet(event: any) {
+    this.setState({ email: event.target.value });
+  }
+
+  passwordSet(event: any) {
+    this.setState({ password: event.target.value });
+  }
+
   render() {
     const { email, password } = this.state;
     return (
       <>
-        <input id="email" type="email" placeholder="メールアドレスを入力" value={email} />
-        <input id="password" type="password" placeholder="パスワードを入力" value={password} />
+        <input id="email" type="email" placeholder="メールアドレスを入力" value={email} onChange={(event) => this.emailSet(event)} />
+        <input id="password" type="password" placeholder="パスワードを入力" value={password} onChange={(event) => this.passwordSet(event)} />
         <button type="button" onClick={this.Login}>Login</button><button type="button">Logout</button>
         {email}
       </>
